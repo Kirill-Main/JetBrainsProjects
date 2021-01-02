1ab9bf4c2ffebf01af06ab2304219c1c16b14e9a
Kirill-Main <kir20-01@ya.ru>
2021-01-02 16:23:58 +0300

    Initial

---
+++ RiderProjects/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Security.Principal;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Привет мир!");
+            
+            var obj = new MySecondClass(20, "Вася");
+            
+            Console.WriteLine(obj.name, obj.age);
+        }
+    }
+
+    internal class MyClass
+    {
+        public string name;
+
+        public MyClass(string name="")
+        {
+            this.name = name;
+        }
+        
+    }
+
+    internal class MySecondClass : MyClass
+    {
+        public int age;
+        
+        public MySecondClass(int age, string name = "")
+            : base(name)
+        {
+            this.age = age;
+        }
+    }
+}
